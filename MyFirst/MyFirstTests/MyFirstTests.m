6464c512504616310beb77301b59275798f32b30
ibokan <ibokan@Jonny.local>
2014-05-14 10:18:35 +0800

    第一次提交

---
+++ MyFirst/MyFirstTests/MyFirstTests.m
@@ -0,0 +1,34 @@
+//
+//  MyFirstTests.m
+//  MyFirstTests
+//
+//  Created by ibokan on 14-5-14.
+//  Copyright (c) 2014年 ibokan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyFirstTests : XCTestCase
+
+@end
+
+@implementation MyFirstTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
